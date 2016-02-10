2a2c1cef84f7b7b4fa72f3386d0cd501ce2ae4d3
Simon Garnier <simon.garnier@gmail.com>
2016-02-10 15:57:57 -0500

    Moved global variables to server side to avoid problems when reloading app page.

--- inst/app/global.R
+++ inst/app/global.R
@@ -4,11 +4,3 @@ library(data.table)
 library(dplyr)
 library(lubridate)
 library(RColorBrewer)
-
-dat <- NULL
-# cbf <- RColorBrewer::brewer.pal(8, "Accent")
-cbf <- c("#A08327", "#B865E9", "#4688A6", "#D43B6B", "#3B9465", "#83547F",
-  "#DD4031", "#58A735", "#607BC2", "#C3692C", "#CB3B95", "#B77FCC", "#5C7A2A",
-  "#B6514E", "#6B6CDC", "#CF6C99", "#8F47A4", "#DC49D5")
-currentTime <- NULL
-proxy <- NULL
--- inst/app/server.R
+++ inst/app/server.R
@@ -2,6 +2,13 @@ options(shiny.maxRequestSize = 10 * 1024 ^ 2)
 
 shinyServer(function(input, output, session) {
 
+  dat <- NULL
+  # cbf <- RColorBrewer::brewer.pal(8, "Accent")
+  cbf <- c("#A08327", "#B865E9", "#4688A6", "#D43B6B", "#3B9465", "#83547F",
+           "#DD4031", "#58A735", "#607BC2", "#C3692C", "#CB3B95", "#B77FCC", "#5C7A2A",
+           "#B6514E", "#6B6CDC", "#CF6C99", "#8F47A4", "#DC49D5")
+  proxy <- NULL
+
   ### Reactive values ###
   react <- reactiveValues(newTrack = 0)
   ### ###
